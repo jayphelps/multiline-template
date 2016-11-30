9c17fba51c59325f1eced5bf17bc5791660f8955
Jay Phelps <hello@jayphelps.com>
2016-11-29 16:08:43 -0800

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
+'use strict';
 var slice = Array.prototype.slice;
 
 function multiline(strings) {
   var args = slice.call(arguments, 1);
 
   return strings
-    // Add interpolated arguments being sure to
-    // indent each line to match
-    .reduce((out, part, i) => {
-      const lines = part.split('\n');
-      
+    // add interpolated arguments
+    .reduce(function (out, part, i) {
       if (args.hasOwnProperty(i)) {
-        const indent = lines[lines.length - 1].replace(/[ \t\r]*\| ([ \t\r]*).*$/, '$1');
-        const arg = args[i] || '';
-        return out + part + arg.split('\n').join('\n' + indent);
+        var lines = part.split('\n');
+        // find indention of the current line
+        var indent = lines[lines.length - 1].replace(/[ \t\r]*\| ([ \t\r]*).*$/, '$1');
+        // indent interpolated lines to match
+        var tail = (args[i] || '').split('\n').join('\n' + indent);
+        return out + part + tail;
       } else {
         return out + part;
       }
